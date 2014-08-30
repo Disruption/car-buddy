eeec93282197818200ec339b36224889cb2c3a42
Sergio Tomas <sergio.tomas@quandoo.de>
2014-08-30 12:12:02 +0200

    Separated the layouts for the three paged fragments

--- app/src/main/java/es/disruption/carbuddy/ui/fragment/MonitorFragment.java
+++ app/src/main/java/es/disruption/carbuddy/ui/fragment/MonitorFragment.java
@@ -22,6 +22,6 @@ public class MonitorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_monitor, container, false);
     }
 }
